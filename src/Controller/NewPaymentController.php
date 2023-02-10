6754d774268e17f6311ebbe83f991cf78152814f
Farid Kuliev <f.farenco@gmail.com>
2023-02-10 21:19:22 +0400

    Third commit

--- src/Controller/NewPaymentController.php
+++ src/Controller/NewPaymentController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Accounts;
+use App\Entity\Cards;
+use App\Entity\UtilityServices;
 use App\Service\TokenService;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,14 +25,25 @@ class NewPaymentController extends AbstractController
     {
         $authorizationHeader = $request->headers->get("Authorization");
         $token = $this->tokenService->decodeToken(substr($authorizationHeader, 7));
+        $em = $doctrine->getManager();
         $email = $token->data['0'];
 
+        $cards = $em->getRepository(Cards::class)->findBy(['user_email'=>$email]);
+        $cardsData = [];
+        foreach ($cards as $card){
+            array_push($cardsData, [$card->getCardNumber(), $card->getPaymentSystem(), $card->getBalance()]);
+        };
 
+        $services = $em->getRepository(UtilityServices::class)->findAll();
+        $servicesData = [];
+        foreach ($services as $service){
+            array_push($servicesData, $service->getServiceName());
+        }
 
         return $this->json([
             'success' => true,
-
-
+            'cards' => $cardsData,
+            'services' => $servicesData
         ]);
     }
 }
